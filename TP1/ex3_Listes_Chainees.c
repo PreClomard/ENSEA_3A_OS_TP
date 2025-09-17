7de9fc477ecbb57a13079f4eb209192cfa007b89
Clovis Pressard <pcetudeing@gmail.com>
2025-09-17 11:59:04 +0200

    Start of the ex3

---
+++ TP1/ex3_Listes_Chainees.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct liste{
+	int value;
+	struct liste *pos;
+} liste;
+
+/*
+* Name: generateList
+* use : generate the list with the intigers
+*/
+void generateList(int size){
+	
+}
+
+int main(){
+	
+	liste *MyList=NULL;
+	liste *element(sizeof(liste));
+	
+	int n=10;
+	for(int i=0; i<n;i++){
+		
+	}
+}
