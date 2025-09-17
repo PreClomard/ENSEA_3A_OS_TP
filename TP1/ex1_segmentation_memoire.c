5727743014dbf44166c4716ffff3325a100e7cea
Clovis Pressard <pcetudeing@gmail.com>
2025-09-17 11:19:33 +0200

    Update of the ex1

--- TP1/ex1_segmentation_memoire.c
+++ TP1/ex1_segmentation_memoire.c
@@ -5,7 +5,7 @@
 #include <sys/mman.h>
 
 
-//Variables
+//Global Variables
 int dataGlobalInit=10;
 int dataGlobalNonInit;
 char* str="Hello world";
@@ -15,15 +15,43 @@ char* str="Hello world";
 * Name: ShowAddr
 * Usel: Show the different address of the segments memories
 **/
-void showAddr(){
-	printf("Address of the Data : %p \n",&dataGlobalInit);  
-        printf("Address of the BSS : %p \n",&dataGlobalNonInit);
-        printf("Address of the Str : %p \n",str);  
+void showAddr();
 
- 
- 
-}
-int main(){
+int main(){	
 	showAddr();
 exit(EXIT_SUCCESS);
+}
+
+void showAddr(){
+        pid_t pid = fork();
+        if(pid>0){
+
+                //Local variables
+                int dataLocal=5;
+                int *dynamic = malloc(sizeof(int));
+                char valuePid[20];
+		
+		void *mmap_ptr = mmap(NULL, sysconf(_SC_PAGESIZE), 
+		PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1,0);
+                
+		printf("Address of the Data : %p \n",&dataGlobalInit);
+                printf("Address of the BSS : %p \n",&dataGlobalNonInit);
+                printf("Address of the Str : %p \n",str);  
+                printf("Address of the Heap : %p \n",dynamic);
+                printf("Address of the Stack : %p \n",&dataLocal); 
+                printf("Address of the Main Function : %p \n",main);
+                printf("Address of the LibC Function : %p \n",printf);
+		printf("Address of the memory mmap (Mmap) : %p\n",mmap_ptr);
+
+                snprintf(valuePid, sizeof(valuePid), "%d",getpid());
+        /*
+        * Note perso: Eviter d'utiliser system préféré l'utilisation
+        * d'excv, la raison est que system exécute qu'importe ce qui
+        * est en son sein ce qui peut être source d'erreur.
+        * code : system(cmd);
+        */
+                char *argv[] = {"pmap","-X",valuePid, NULL};
+                execv("/usr/bin/pmap",argv);
+
+        }
 }
