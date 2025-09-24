f5f94ca42b61e3fe9610226c7ed56ed372dabc80
NathGrou <161328538+NathGrou@users.noreply.github.com>
2025-09-24 10:50:55 +0200

    Mise à jour de la question1

--- TP2/Question1/allocateur.c
+++ TP2/Question1/allocateur.c
@@ -1,52 +1,54 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stddef.h>
+#include <stdint.h>
 #include "allocateur.h"
 
-#define  MAGIC_NUMBER 0x012345678ABDEFL
+#define MAGIC_NUMBER 0x0123456789ABCDEFULL
 
 typedef struct block {
-    int size;
+    size_t size;
     struct block *next;
-    int magic;
+    uint64_t magic;
     char data[1];
 } block_t;
 
 static block_t *free_list = NULL;
 
 void init_heap() {
-	free_list = NULL;
+    free_list = NULL;
 }
 
 void *malloc_3is(size_t size) {
-	if (size <=0){
-	  return NULL;
-	}
+    if (size <= 0) {
+        return NULL;
+    }
 
-	block_t *prev = NULL;
-	block_t *curr = free_list;
+    block_t *prev = NULL;
+    block_t *curr = free_list;
 
-	while (curr) {
-	    if (curr->size >= size) {
-		if (curr->size > size + sizeof(block_t) + 16){ 
-		   block_t *new_block = (block_t *)((char *)curr + sizeof(block_t) + size);
-		   new_block->size = curr->size -  size - sizeof(block_t);
-		   new_block->next = curr->next;
-		   new_block->magic = MAGIC_NUMBER;
-		   curr->size = size;
-		   curr->next = new_block;
-		} else {
-		   if (prev) {
-			prev->next = curr->next;
-		   } else {
-			free_list = curr->next;
-	   	   }
-		}
-		curr->magic = MAGIC_NUMBER;
-		return &(curr->data);
-	 }
-	 prev = curr;
-	 curr = curr->next;
+    while (curr) {
+        if (curr->size >= size) {
+            if (curr->size > size + sizeof(block_t) + 16) {
+                block_t *new_block = (block_t *)((char *)curr + sizeof(block_t) + size);
+                new_block->size = curr->size - size - sizeof(block_t);
+                new_block->next = curr->next;
+                new_block->magic = MAGIC_NUMBER;
+                curr->size = size;
+                curr->next = new_block;
+            } else {
+                if (prev) {
+                    prev->next = curr->next;
+                } else {
+                    free_list = curr->next;
+                }
+            }
+            curr->magic = MAGIC_NUMBER;
+            return &(curr->data);
+        }
+        prev = curr;
+        curr = curr->next;
+    }
 
     size_t total_size = sizeof(block_t) + size;
     block_t *new_block = (block_t *)sbrk(total_size);
@@ -59,7 +61,16 @@ void *malloc_3is(size_t size) {
     return &(new_block->data);
 }
 
-
 void free_3is(void *ptr) {
     return;
 }
+
+int main() {
+    init_heap();
+    void *ptr1 = malloc_3is(10);
+    void *ptr2 = malloc_3is(20);
+    void *ptr3 = malloc_3is(30);
+    printf("Allocations réussies : %p, %p, %p\n", ptr1, ptr2, ptr3);
+    return 0;
+}
+
--- TP2/Question1/allocateur.h
+++ TP2/Question1/allocateur.h
@@ -2,16 +2,17 @@
 #define ALLOCATEUR_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 typedef struct HEADER_TAG {
 	struct HEADER_TAG *ptr_next;
 	size_t bloc_size;
-	long magic_number;
+	uint64_t magic_number;
 } HEADER;
 
 
 void init_heap();
-void *malloc_3is(int size);
+void *malloc_3is(size_t size);
 void free_3is(void *ptr);
 
 #endif
